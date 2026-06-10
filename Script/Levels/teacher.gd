597defb397175a32bfae4bc502ef938de1b45731
basliyalaaryan-maker <basliyalaaryan@gmail.com>
2026-06-10 12:28:42 +1200

    made a teacher that after collecting books by player so teacher can ensure it that book i collected and level is being completed

--- Script/Levels/teacher.gd
+++ Script/Levels/teacher.gd
@@ -8,7 +8,20 @@ func _process(_delta):
 
 	# If player is near the teacher and presses E
 	if player_near and Input.is_action_just_pressed("interact"):
-		print("Teacher: Collect all 3 books!")
+
+		# Find the Player node
+		var player = get_node("../Player")
+
+		# Check how many books have been collected
+		if player.books_collected >= 3:
+			print("Teacher: Good job!")
+			print("Level 1 Complete!")
+
+			# Later we can load Level 2 here
+			# get_tree().change_scene_to_file("res://Scean/Level/level2.tscn")
+
+		else:
+			print("Teacher: Collect all 3 books!")
 
 
 # Runs when something enters the teacher's Area2D
@@ -16,8 +29,6 @@ func _on_area_2d_body_entered(body):
 
 	# Check if it is the player
 	if body.name == "Player":
-
-		# Allow interaction with teacher
 		player_near = true
 
 
@@ -26,6 +37,4 @@ func _on_area_2d_body_exited(body):
 
 	# Check if it is the player
 	if body.name == "Player":
-
-		# Stop interaction with teacher
 		player_near = false
