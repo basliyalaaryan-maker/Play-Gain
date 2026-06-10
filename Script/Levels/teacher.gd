907d93bddb4a3c30119609d1e14545d3c0c94512
basliyalaaryan-maker <basliyalaaryan@gmail.com>
2026-06-11 10:10:01 +1200

    i done the teacher and when player collected all 3 book then teacher msg appears to god job level 1 completed and its can continue to level 2

--- Script/Levels/teacher.gd
+++ Script/Levels/teacher.gd
@@ -1,40 +1,49 @@
 extends CharacterBody2D
 
-# Checks if the player is close enough to talk to the teacher
+# Get the TeacherLabel from the game scene
+@onready var teacher_label = get_node("../HUD/TeacherLabel")
+
+# Checks if the player is close enough to the teacher
 var player_near = false
 
 
 func _process(_delta):
 
-	# If player is near the teacher and presses E
+	# If the player is near the teacher and presses E
 	if player_near and Input.is_action_just_pressed("interact"):
 
-		# Find the Player node
+		# Get the Player node
 		var player = get_node("../Player")
 
-		# Check how many books have been collected
+		# Check if the player has collected all 3 books
 		if player.books_collected >= 3:
-			print("Teacher: Good job!")
-			print("Level 1 Complete!")
+
+			# Show success message
+			teacher_label.visible = true
+			teacher_label.text = "🎉Good Job!🎉\nLevel 1 Complete!🏆\nPress E to continue."
 
 			# Later we can load Level 2 here
 			# get_tree().change_scene_to_file("res://Scean/Level/level2.tscn")
 
 		else:
-			print("Teacher: Collect all 3 books!")
+
+			# Tell the player to collect all books
+			teacher_label.visible = true
+			teacher_label.text = "Collect all 3 books first!"
 
 
-# Runs when something enters the teacher's Area2D
+# Runs when the player enters the teacher's Area2D
 func _on_area_2d_body_entered(body):
 
-	# Check if it is the player
 	if body.name == "Player":
 		player_near = true
 
 
-# Runs when something leaves the teacher's Area2D
+# Runs when the player leaves the teacher's Area2D
 func _on_area_2d_body_exited(body):
 
-	# Check if it is the player
 	if body.name == "Player":
 		player_near = false
+
+		# Hide the message
+		teacher_label.visible = false
